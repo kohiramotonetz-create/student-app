625cd4644c6840aaa9b0744efb2caf4dffe99341
kohiramotonetz-create <ko.hiramoto.netz@gmail.com>
2026-04-02 17:20:43 +0900

    タイトル修正

--- src/App.jsx
+++ src/App.jsx
@@ -214,7 +214,7 @@ function App() {
       
       {step === 'login' && (
         <div className="login-box">
-          <h1>生徒単語アプリ</h1>
+          <h1>生徒単語アプリ💯</h1>
           <input type="text" placeholder="生徒番号" value={userId} onChange={(e) => setUserId(e.target.value)} />
           <input type="password" placeholder="パスワード(初期:1234)" value={password} onChange={(e) => setPassword(e.target.value)} />
           <button onClick={handleLogin}>ログイン</button>
